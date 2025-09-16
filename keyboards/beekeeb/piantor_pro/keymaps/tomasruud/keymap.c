41140587c63f97c7fd4a8d06d7284a3211cf7fce
Tomas Ruud <tru@axogroup.com>
2025-09-16 11:08:49 +0200

    Add esc as osm cancel key

--- keyboards/beekeeb/piantor_pro/keymaps/tomasruud/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/tomasruud/keymap.c
@@ -67,10 +67,18 @@ bool sw_app_active = false;
 bool sw_win_active = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  update_swapper(&sw_app_active, KC_LGUI, KC_TAB, SW_APP, keycode, record);
-  update_swapper(&sw_win_active, KC_LGUI, NO_LABK, SW_WIN, keycode, record);
+    update_swapper(&sw_app_active, KC_LGUI, KC_TAB, SW_APP, keycode, record);
+    update_swapper(&sw_win_active, KC_LGUI, NO_LABK, SW_WIN, keycode, record);
 
-  return true;
+    if (record->event.pressed && keycode == KC_ESC) {
+        // If any one-shot modifiers are active, cancel them
+        if (get_oneshot_mods()) {
+            clear_oneshot_mods();
+            return false; // swallow Esc (use it purely as "cancel")
+        }
+    }
+
+    return true;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
