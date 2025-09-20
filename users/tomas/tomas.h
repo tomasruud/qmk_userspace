d71765924739958da643d9d851bb9359baa54fe5
Tomas Ruud <git@me.2ms.no>
2025-09-20 13:03:08 +0200

    Split norwegian ansi stuff into separate header

--- users/tomas/tomas.h
+++ users/tomas/tomas.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "quantum.h"
+#include "norwegian-ansi.h"
 
 enum layers {
     DEF,
@@ -10,91 +11,14 @@ enum layers {
     FUN,
 };
 
-// Key definitions for Norwegian macOS layout using ANSI layout.
-#define NO_1 KC_1             // 1
-#define NO_2 KC_2             // 2
-#define NO_3 KC_3             // 3
-#define NO_4 KC_4             // 4
-#define NO_5 KC_5             // 5
-#define NO_6 KC_6             // 6
-#define NO_7 KC_7             // 7
-#define NO_8 KC_8             // 8
-#define NO_9 KC_9             // 9
-#define NO_0 KC_0             // 0
-#define NO_Q KC_Q             // Q
-#define NO_A KC_A             // A
-#define NO_B KC_B             // B
-#define NO_C KC_C             // C
-#define NO_D KC_D             // D
-#define NO_E KC_E             // E
-#define NO_F KC_F             // F
-#define NO_G KC_G             // G
-#define NO_H KC_H             // H
-#define NO_I KC_I             // I
-#define NO_J KC_J             // J
-#define NO_K KC_K             // K
-#define NO_L KC_L             // L
-#define NO_M KC_M             // M
-#define NO_N KC_N             // N
-#define NO_O KC_O             // O
-#define NO_P KC_P             // P
-#define NO_R KC_R             // R
-#define NO_S KC_S             // S
-#define NO_T KC_T             // T
-#define NO_U KC_U             // U
-#define NO_V KC_V             // V
-#define NO_W KC_W             // W
-#define NO_X KC_X             // X
-#define NO_Y KC_Y             // Y
-#define NO_Z KC_Z             // Z
-#define NO_AE KC_QUOT         // Æ
-#define NO_OSTR KC_SCLN       // Ø
-#define NO_ARNG KC_LBRC       // Å
-#define NO_LCBR S(A(KC_8))    // {
-#define NO_RCBR S(A(KC_9))    // }
-#define NO_LPAR S(KC_8)       // (
-#define NO_RPAR S(KC_9)       // )
-#define NO_LBRC A(KC_8)       // [
-#define NO_RBRC A(KC_9)       // ]
-#define NO_LABK KC_GRV        // <
-#define NO_RABK S(KC_GRV)     // >
-#define NO_BLSH S(A(KC_7))    // (backslash)
-#define NO_PIPE A(KC_7)       // |
-#define NO_SLSH S(KC_7)       // /
-#define NO_TILD A(KC_RBRC)    // ~ (dead)
-#define NO_QUOT KC_NUBS       // '
-#define NO_DQOU S(KC_2)       // "
-#define NO_HASH S(KC_3)       // #
-#define NO_AT KC_BSLS         // @
-#define NO_DLR S(KC_4)        // $
-#define NO_PERC S(KC_5)       // %
-#define NO_AMPR S(KC_6)       // &
-#define NO_ASTR S(NO_AT)      // *
-#define NO_MINS KC_SLSH       // -
-#define NO_USCR S(NO_MINS)    // _
-#define NO_CLON S(KC_DOT)     // :
-#define NO_COMM KC_COMM       // ,
-#define NO_SEMI LSFT(NO_COMM) // ;
-#define NO_DOT KC_DOT         // .
-#define NO_COMM KC_COMM       // ,
-#define NO_EXLM S(NO_1)       // !
-#define NO_ACUT KC_EQL        // ` (dead)
-#define NO_GRV S(KC_EQL)      // ´ (dead)
-#define NO_CIRC S(KC_RBRC)    // ^ (dead)
-#define NO_UMLA KC_RBRC       // ¨ (dead)
-#define NO_EQL S(KC_0)        // =
-#define NO_PLUS KC_MINS       // +
-#define NO_QUES S(KC_MINS)    // ?
-
-#define EMOJI G(C(KC_SPC)) // Emoji
-
-#define TR_TMUX C(NO_A) // Tmux trigger
-#define TR_SCDN C(NO_D) // Scroll down
-#define TR_SCUP C(NO_U) // Scroll up
-#define TR_JMPF C(NO_I) // Jump forwards
-#define TR_JMPB C(NO_O) // Jump backwards
-#define TR_COPY G(NO_C) // Copy
-#define TR_PAST G(NO_V) // Paste
+#define TR_EMOJ G(C(KC_SPC))  // Emoji
+#define TR_TMUX C(NO_A)       // Tmux trigger
+#define TR_SCDN C(NO_D)       // Scroll down
+#define TR_SCUP C(NO_U)       // Scroll up
+#define TR_JMPF C(NO_I)       // Jump forwards
+#define TR_JMPB C(NO_O)       // Jump backwards
+#define TR_COPY G(NO_C)       // Copy
+#define TR_PAST G(NO_V)       // Paste
 #define TR_WCTR C(S(A(NO_G))) // Center window
 #define TR_WGRW C(S(A(NO_M))) // Maximise window
 
@@ -117,7 +41,7 @@ NO_Z    ,NO_X    ,NO_C    ,NO_V    ,NO_B    ,        NO_N    ,NO_M    ,NO_ARNG ,
 #define SPLIT_NAV_LAYER(APPLY) APPLY(                                                             \
 XXXXXXX ,XXXXXXX ,SW_PTAB ,SW_NTAB ,CW_TOGG ,        KC_ESC  ,KC_PGDN ,KC_PGUP ,TR_JMPF ,TR_JMPB ,\
 OS_LALT ,OS_LGUI ,OS_LCTL ,OS_LSFT ,TR_COPY ,        KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT ,XXXXXXX ,\
-EMOJI   ,SW_WIN  ,SW_APP  ,TR_TMUX ,TR_PAST ,        KC_TAB  ,KC_HOME ,KC_END  ,TR_WCTR ,TR_WGRW ,\
+TR_EMOJ ,SW_WIN  ,SW_APP  ,TR_TMUX ,TR_PAST ,        KC_TAB  ,KC_HOME ,KC_END  ,TR_WCTR ,TR_WGRW ,\
                                     _______ ,     LT(NUM, KC_ENT)                                 \
 )
 
